3e7376ce110eb7865d4d77aa91807bdc51a75164
Pierre Ferreira <pierre@tektite.biz>
2017-02-20 11:39:58 +0200

    HTTP request to googleapis

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const request = require('request')
+const yargs = require('yargs')
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+// console.log(argv)
+let encodedAddress = encodeURIComponent(argv.a)
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=761%2011th%20ave%20Wonderboom%20south%20Pretoria',
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+encodedAddress,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2))
+  // console.log(JSON.stringify(body, undefined, 2))
+  console.log(`Address: ${body.results[0].formatted_address}`)
+  console.log(`lat: ${body.results[0].geometry.location.lat}`)
+  console.log(`lng: ${body.results[0].geometry.location.lng}`)
+  // console.log('-----------')
+  // console.log('ERROR:::', JSON.stringify(error, undefined, 2))
+  // console.log('-----------')
+  // console.log('RESPONSE:::', JSON.stringify(response, undefined, 2))
 })
