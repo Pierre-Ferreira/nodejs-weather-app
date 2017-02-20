25f7ab02b42b61f79266fe277689bb837181aef7
Pierre Ferreira <pierre@tektite.biz>
2017-02-20 16:14:15 +0200

    Error handling of HTTP request

--- app.js
+++ app.js
@@ -21,12 +21,13 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+encodedAddress,
   json: true
 }, (error, response, body) => {
-  // console.log(JSON.stringify(body, undefined, 2))
-  console.log(`Address: ${body.results[0].formatted_address}`)
-  console.log(`lat: ${body.results[0].geometry.location.lat}`)
-  console.log(`lng: ${body.results[0].geometry.location.lng}`)
-  // console.log('-----------')
-  // console.log('ERROR:::', JSON.stringify(error, undefined, 2))
-  // console.log('-----------')
-  // console.log('RESPONSE:::', JSON.stringify(response, undefined, 2))
+  if (error) {
+    console.log('Unable to connect to Google servers!')
+  } else if (body.status === "ZERO_RESULTS") {
+      console.log('Unable to find that address')
+  } else if (body.status === "OK") {
+      console.log(`Address: ${body.results[0].formatted_address}`)
+      console.log(`lat: ${body.results[0].geometry.location.lat}`)
+      console.log(`lng: ${body.results[0].geometry.location.lng}`)
+  }
 })
